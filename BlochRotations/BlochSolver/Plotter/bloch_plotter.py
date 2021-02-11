Plotting fixed, new hamiltonian and rotation matrix added

BlochRotations/BlochSolver/Plotter/bloch_plotter.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import warnings
+
 warnings.filterwarnings('ignore')
 
 
@@ -27,9 +28,9 @@ class BlochPlotter:
             return
 
     @staticmethod
-    def __plot_evolution(pulse_vectors: np.array, init_vector: np.array, target_state:np.array):
+    def __plot_evolution(pulse_vectors: np.array, init_vector: np.array, target_state: np.array):
 
-        phi, theta = np.mgrid[0:2*np.pi:30j, 0:np.pi:30j]
+        phi, theta = np.mgrid[0:2 * np.pi:20j, 0:np.pi:20j]
         x_cor, y_cor, z_cor = pulse_vectors
         x_init, y_init, z_init = init_vector
         x_target, y_target, z_target = target_state
@@ -40,11 +41,15 @@ class BlochPlotter:
 
         fig = plt.figure(figsize=(17, 12))
         ax = fig.gca(projection='3d')
-        ax.plot_wireframe(x_sphere, y_sphere, z_sphere, color="darkgrey", alpha=0.1)
-        ax.scatter(x_init, y_init, z_init, color="blue", s=40)
-        ax.scatter(x_target, y_target, z_target, color="green", s=40)
-        ax.scatter(x_cor[-1], y_cor[-1], z_cor[-1], color="black", s=40)
-        ax.plot3D(x_cor, y_cor, z_cor, "--D", color="red")
+        ax.plot_wireframe(x_sphere, y_sphere, z_sphere, color="black", alpha=0.1)
+        ax.scatter(x_init, y_init, z_init, color="blue", s=40, label='Init state')
+        ax.scatter(x_target, y_target, z_target, color="green", s=40, label='Final target point')
+        ax.scatter(x_cor[-1], y_cor[-1], z_cor[-1], color="black", s=40, label='Last pulse state')
+        ax.plot3D(x_cor, y_cor, z_cor, "--D", color='red', label="Pulse evolution")
+        ax.set_xlabel('$X$', fontsize=20)
+        ax.set_ylabel('$Y$', fontsize=20)
+        ax.set_zlabel(r'$Z$', fontsize=20, )
+        ax.legend()
         plt.show()
 
         return

BlochRotations/BlochSolver/Plotter/plotter_converter.py
@@ -7,23 +7,20 @@ class PlotterConverter(rh.RotationHandler, nm.NumericalMethods):
 
     @classmethod
     def convert_bloch_coordinates(cls, pulses: np.array, init_state: np.array, target_state: np.array):
+        pulses = pulses[::-1]
         bloch_states = cls.get_pulse_states(pulses, init_state)
         init_vector = cls.get_real_vector(init_state)
         target_final_state = cls.get_real_vector(target_state)
         real_vectors = cls.get_pulse_real_vectors(bloch_states)
         real_vectors = np.concatenate((np.array([init_vector]), real_vectors), axis=0)
-        return init_vector, target_final_state, real_vectors.T
+        return init_vector.T, target_final_state.T, real_vectors.T
 
     @classmethod
     def get_real_vector(cls, bloch_state: np.array):
-        alpha = 2 * np.arccos(np.real(bloch_state[0]))
-        if alpha == 0:
-            phi = 0
-        else:
-            phi = np.real(- 1j * np.log(bloch_state[1] / np.sin(alpha / 2)))
-        x = np.cos(phi) * np.sin(alpha)
-        y = np.sin(phi) * np.sin(alpha)
-        z = np.cos(alpha)
+        density_m = cls.get_density_operator(bloch_state)
+        x = 2 * np.real(density_m[0][1])
+        y = 2 * np.imag(density_m[1][0])
+        z = density_m[0][00] - density_m[1][1]
         return np.array([x, y, z])
 
     @classmethod
@@ -32,10 +29,11 @@ class PlotterConverter(rh.RotationHandler, nm.NumericalMethods):
 
     @classmethod
     def get_pulse_states(cls, pulses: np.array, init_state: np.array):
-        N = len(pulses)
+        n = len(pulses)
         pulse_operators = cls.get_pulse_operators(pulses)
         evolution_operators = np.array([cls.get_evolution(pulse_operators[:step + 1])
-                                        for step in range(N)])
+                                        for step in range(n)])
+
         bloch_states = np.array([cls.get_state(evolution_operator, init_state)
                                  for evolution_operator in evolution_operators])
         return bloch_states

BlochRotations/BlochSolver/QuantumSolvers/numerics/numerical_methods.py
@@ -3,14 +3,14 @@ import numpy as np
 
 class NumericalMethods:
     dt = None
-    ctr_h = None
     h_bar = None
+    h_k = None
 
     @classmethod
-    def load_numerical_settings(cls, settings: dict, control_hamiltonian:np.array):
+    def load_numerical_settings(cls, control_hamiltonian: np.array, settings: dict):
         cls.dt = settings["pulse_time"]
         cls.h_bar = settings["h_bar"]
-        cls.ctr_h = control_hamiltonian
+        cls.h_k = control_hamiltonian
         return
 
     @classmethod
@@ -27,9 +27,10 @@ class NumericalMethods:
 
     @classmethod
     def get_gradient(cls, back_operators: np.array, prop_operators: np.array):
-        grad = -1 * np.array([cls.get_matrix_product(back_op, 1j * (cls.dt / cls.h_bar) * cls.get_commutator(cls.ctr_h,
+        grad = -1 * np.array([cls.get_matrix_product(back_op, 1j * (cls.dt / cls.h_bar) * cls.get_commutator(cls.h_k,
                                                                                                              prop_op))
                               for back_op, prop_op in zip(back_operators, prop_operators)])
+
         return grad
 
     @classmethod

BlochRotations/BlochSolver/QuantumSolvers/rotations/rotation_handler.py
@@ -3,9 +3,12 @@ from BlochSolver.Utils import settings as s
 from BlochSolver.QuantumSolvers.numerics import numerical_methods as nm
 
 
-# TODO: Maybe different instances will be needed for multiprocessing
 class RotationHandler:
     idn = np.identity(2)
+    # s.settings["time_tc"] = 1.5e-5
+    # s.settings["magnetic_field"] = 0.001
+    # s.settings["pulse_time"] = 2.41 * 10 **(-9)
+    # s.settings["dg_factor"] = 1.71
 
     @classmethod
     def __get_pulse_detuning(cls, pulse: float):
@@ -24,11 +27,18 @@ class RotationHandler:
     # TODO: Verify rotation operator !
     @classmethod
     def __get_evolution_operator(cls, pulse: float):
-        arg = (s.settings["pulse_time"] / s.settings["h_bar"]) * \
-              (s.settings["dg_factor"] * s.settings["bohr_magneton"] * s.settings["magnetic_field"])* 0.5
-        j_arg = (s.settings["pulse_time"] / s.settings["h_bar"]) * cls.__get_pulse_detuning(pulse)
-        return np.array([[np.cos(arg * 0.5) * np.exp(-1j * j_arg * 0.5), -1j * np.sin(arg)],
-                         [-1j * np.sin(arg), np.cos(arg * 0.5) * np.exp(1j * j_arg * 0.5)]])
+        j_f = cls.__get_pulse_detuning(pulse)
+        f_term = s.settings["dg_factor"] * s.settings["bohr_magneton"] * s.settings["magnetic_field"]
+        omega = np.sqrt(j_f**2 + f_term**2)
+        phi = (omega * s.settings["pulse_time"]) / (2 * s.settings["h_bar"])
+        alpha = - j_f / omega
+        beta = f_term / omega
+        return np.array([[np.cos(phi) + 1j * alpha * np.sin(phi), -1j * beta * np.sin(phi)],
+                         [-1j * beta * np.sin(phi), np.cos(phi) - 1j * alpha * np.sin(phi)]])
+
+    @classmethod
+    def get_control_hamiltonian(cls):
+        return cls.__get_pauli_z()
 
     @classmethod
     def get_pulse_operators(cls, pulses: np.array):
@@ -90,3 +100,15 @@ class RotationHandler:
     def __get_z_rotation(alpha: float):
         return np.array([[np.exp(-1j * alpha / 2), 0],
                          [0, np.exp(1j * alpha / 2)]])
+
+    @staticmethod
+    def __get_pauli_x():
+        return np.array([[0, 1], [1, 0]])
+
+    @staticmethod
+    def __get_pauli_y():
+        return np.array([[0, -1j], [1j, 0]])
+
+    @staticmethod
+    def __get_pauli_z():
+        return np.array([[1, 0], [0, -1]])

BlochRotations/BlochSolver/QuantumSolvers/solvers/quantum_grape.py
@@ -13,11 +13,10 @@ class QuantumGrape(rh.RotationHandler, nm.NumericalMethods):
         self.__settings = settings.settings
         self.__numerical_settings = settings.numerical_settings
         self.__l_rate = self.__numerical_settings["learning_rate"]
-        self.__get_control_hamiltonian()
-        self.load_numerical_settings(self.__settings, self.__ctrl_h)
+        h_k = self.get_control_hamiltonian()
+        self.load_numerical_settings(h_k, self.__settings)
         self.__sc.load_control_settings(self.__numerical_settings)
 
-        self.__ctrl_h = None
         self.__n_steps = None
         self.__target_operator = None
         self.__ideal_state = None
@@ -47,8 +46,7 @@ class QuantumGrape(rh.RotationHandler, nm.NumericalMethods):
         iteration = 0
         while True:
             propagation_operators, backward_operators = self.__get_operators(initial_state)
-            self.__e_e = self.__e_s + self.__l_rate * self.get_gradient(backward_operators, propagation_operators)
-            # self.__e_e = np.clip(self.__e_e, a_min=0.0015, a_max=0.004)
+            self.__e_e = self.__e_s + (self.__l_rate * self.get_gradient(backward_operators, propagation_operators))
 
             self.__get_order(self.__e_e)
             status_f, fidelity = self.__evaluate_fidelity(initial_state)
@@ -77,8 +75,9 @@ class QuantumGrape(rh.RotationHandler, nm.NumericalMethods):
         while True:
             propagation_operators, backward_operators = self.__get_operators(initial_state)
             _, fidelity_s = self.__evaluate_fidelity(initial_state)
-            self.__e_e = self.__e_s + self.__l_rate * self.get_gradient(backward_operators, propagation_operators)
+            self.__e_e = self.__e_s - self.__l_rate * self.get_gradient(backward_operators, propagation_operators)
 
+            self.__get_order(self.__e_e)
             status_f, fidelity_e = self.__evaluate_fidelity(initial_state)
             self.__l_rate = self.__sc.update_learning_rate(fidelity_s, fidelity_e, self.__l_rate)
 
@@ -193,10 +192,4 @@ class QuantumGrape(rh.RotationHandler, nm.NumericalMethods):
 
     def __get_order(self, pulses: np.array):
         self.__inv_pulses = pulses[::-1]
-        return
-
-    def __get_control_hamiltonian(self):
-        h = self.idn + np.array([[1, 0], [0, -1]])
-        # self.__ctrl_h = np.kron(h, idn)
-        self.__ctrl_h = h
         return

BlochRotations/BlochSolver/SolversManager/solvers_manager.py
@@ -37,7 +37,7 @@ class Solvers:
               , "\n############################################"
               , " \n ---> Target rotation: ", np.rad2deg(angles), "around: ", axes
               , "\n ---> Initial state:   ",
-              "[", initial_state[0], initial_state[1], "]"
+              "[", np.round(initial_state[0], 3), np.round(initial_state[1], 3), "]"
               , "\n ---> Initial pulses:  ", "[", initial_pulses[0], "...", initial_pulses[0], "]")
         return
 

BlochRotations/main.py
@@ -7,8 +7,8 @@ def main():
     quantum_solvers = solvers_manager.Solvers()
     bloch_plotter = bs.BlochPlotter()
 
-    initial_pulses = 0.004 * np.ones(32)
-    angles = [np.pi / 2]
+    initial_pulses = 0.001 * np.ones(32)
+    angles = [np.pi/2]
     axes = ["x"]
     initial_state = np.array([1, 0])
     target_state, pulses = quantum_solvers.get_solver(solver_type="GRAPE",
@@ -19,7 +19,10 @@ def main():
                                                       axes=axes,
                                                       initial_state=initial_state)
     del quantum_solvers
-    bloch_plotter.plot(plot_type="evolution", pulses_final=pulses, init_state=initial_state, target_state=target_state)
+    bloch_plotter.plot(plot_type="evolution", pulses_final=pulses, init_state=initial_state,
+                       target_state=target_state)
+    # bloch_plotter.plot(plot_type="evolution", pulses_final=initial_pulses, init_state=initial_state,
+    #                    target_state=target_state)
     bloch_plotter.plot(plot_type="pulses", pulses_final=pulses, pulses_init=initial_pulses)
     return
 